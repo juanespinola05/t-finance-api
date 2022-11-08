96d8b00269ffc216cee0bc76b9d7d342aa728d4a
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-11-08 09:42:38 -0300

    setup morgan logger

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const cors = require('cors')
+const morgan = require('morgan')
 const { PORT } = require('./config')
 
 const app = express()
-app.use(express.json())
 
+app.use(morgan('dev'))
+app.use(express.json())
 app.use(cors())
 
 app.get('/', (req, res) => {
