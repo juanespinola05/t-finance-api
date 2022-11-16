ab04f2932d450540eeeb0d1a05f82b31dd8d623e
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-11-16 16:11:02 -0300

    add credentials message

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -1,2 +1,3 @@
 export const EMAIL_EXISTS: string = 'Email is already in use'
 export const INVALID_FIELDS: string = 'Wrong email or password'
+export const INVALID_CREDENTIALS: string = 'Invalid credentials'
