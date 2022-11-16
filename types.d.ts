aaf2d8eb2f1fcb478d1d165c66f704f52e34a347
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-11-16 18:55:44 -0300

    update types

--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,3 @@
-import { JwtPayload } from 'jsonwebtoken'
-
 export interface User {
   id: number
   name: string
@@ -22,6 +20,25 @@ export interface Operation {
   deletedAt: Date
 }
 
+export interface Category {
+  id: number
+  tagname: string
+  createdAt: Date
+  getOperation: HasManyGetAssociationsMixin<Operation>
+  addOperation: HasManyAddAssociationMixin<Operation, number>
+  addOperations: HasManyAddAssociationsMixin<Operation[], number>
+  setOperations: HasManySetAssociationsMixin<Operation, number>
+  removeOperation: HasManyRemoveAssociationMixin<Operation, number>
+  removeOperations: HasManyRemoveAssociationsMixin<Operation, number>
+  hasOperation: HasManyHasAssociationMixin<Operation, number>
+  hasOperations: HasManyHasAssociationsMixin<Operation, number>
+  countOperations: HasManyCountAssociationsMixin
+  createOperation: HasManyCreateAssociationMixin<Operation, 'categoryId'>
+  operations?: NonAttribute<Operation[]>
+}
+
+export interface CategoryOutput extends Required<Category> {}
+
 export type UserCreationAttributes = Omit<User, 'id' | 'createdAt' | 'updatedAt'>
 export type UserLoginAttributes = Pick<User, 'email' | 'password'>
 export type OperationCreationAttributes = Pick<Operation, 'concept' | 'type' | 'amount' | 'date'> & { category: string }
@@ -30,7 +47,14 @@ type UserPayload = Pick<User, 'email'>
 declare global {
   namespace Express {
     export interface Request {
-      user?: JwtPayload | string
+      user: any
+    }
+  }
+
+  namespace Intl {
+    class ListFormat {
+      constructor (locales?: string | string[], options?: Intl.ListFormatOptions)
+      public format: (items?: string[]) => string
     }
   }
 }
