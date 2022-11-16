84ad1c12bdfc87359f582cc24b05486f21a0eb63
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-11-16 16:10:46 -0300

    add operation type

--- types.d.ts
+++ types.d.ts
@@ -1,4 +1,4 @@
-import { Model } from 'sequelize'
+import { JwtPayload } from 'jsonwebtoken'
 
 export interface User {
   id: number
@@ -9,5 +9,28 @@ export interface User {
   updatedAt: Date
 }
 
+export interface Operation {
+  id: number
+  concept: string
+  type: 'income' | 'outflow'
+  amount: number
+  date: Date
+  userId: number
+  categoryId: number
+  createdAt: Date
+  updatedAt: Date
+  deletedAt: Date
+}
+
 export type UserCreationAttributes = Omit<User, 'id' | 'createdAt' | 'updatedAt'>
 export type UserLoginAttributes = Pick<User, 'email' | 'password'>
+export type OperationCreationAttributes = Pick<Operation, 'concept' | 'type' | 'amount' | 'date'> & { category: string }
+
+type UserPayload = Pick<User, 'email'>
+declare global {
+  namespace Express {
+    export interface Request {
+      user?: JwtPayload | string
+    }
+  }
+}
