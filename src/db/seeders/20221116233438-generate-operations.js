dcb4fc56fff35c4bf732c25528c1fef7ac5a14dc
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-11-26 18:24:31 -0300

    fix operations seeder

--- src/db/seeders/20221116233438-generate-operations.js
+++ src/db/seeders/20221116233438-generate-operations.js
@@ -4,17 +4,24 @@ const { faker } = require('@faker-js/faker')
 module.exports = {
   async up (queryInterface, Sequelize) {
     const operations = Array.from({ length: 100 }, () => {
-      return {
+      const operation = {
         concept: faker.lorem.sentence(4),
         type: ['income', 'outflow'][faker.datatype.number({ min: 0, max: 1 })],
         date: faker.date.recent(100),
-        amount: faker.datatype.number(),
         created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null,
-        user_id: 1,
-        category_id: [1, 2, 3, 4][faker.datatype.number({ min: 0, max: 4 })]
+        user_id: 1
       }
+      const isOutflow = operation.type === 'outflow'
+      operation.amount = isOutflow
+        ? faker.datatype.number({ min: -30, max: -1 })
+        : faker.datatype.number({ min: 1, max: 30 })
+
+      operation.category_id = isOutflow
+        ? [1, 2, 3, 4][faker.datatype.number({ min: 0, max: 4 })]
+        : null
+      return operation
     })
     await queryInterface.bulkInsert('operations', [...operations])
   },
