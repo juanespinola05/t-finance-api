7778f83c7267ab20e989994e0ac4c610e9c8f262
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-11-25 22:46:31 -0300

    create updateinput type for operations

--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,4 @@
+import { ForeignKey } from 'sequelize'
 
 export const enum OperationType {
   INCOME = 'income',
@@ -48,6 +49,14 @@ export type UserCreationAttributes = Omit<User, 'id' | 'createdAt' | 'updatedAt'
 export type UserLoginAttributes = Pick<User, 'email' | 'password'>
 export type OperationCreationAttributes = Pick<Operation, 'concept' | 'type' | 'amount' | 'date'> & { category: string }
 export type OperationOutput = Pick<Operation, 'concept' | 'amount' | 'date' | 'createdAt' | 'id' | 'type'>
+
+export interface OperationUpdateInput {
+  concept?: string
+  type?: OperationType
+  amount?: number
+  date?: Date
+  categoryId?: ForeignKey<User, 'id'> | null
+}
 
 type UserPayload = Pick<User, 'email'>
 declare global {
