ddbd79db368ecbe3b01ffc33c31fedc956b6687c
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-11-28 22:09:38 -0300

    fix method return type and use FindOptions instead

--- src/utils/BaseService.ts
+++ src/utils/BaseService.ts
@@ -1,14 +1,12 @@
-import { ModelStatic, WhereOptions } from 'sequelize'
+import { FindOptions, Model, ModelStatic } from 'sequelize'
 
 export default class BaseService<M extends ModelStatic<any>> {
   constructor (
     protected model: M
   ) {}
 
-  async findOne (options: WhereOptions): Promise<M | null> {
-    const foo = await this.model.findOne({
-      where: options
-    })
-    return foo
+  async findOne (options: FindOptions): Promise< Model | null> {
+    const instance = await this.model.findOne(options)
+    return instance
   }
 }
