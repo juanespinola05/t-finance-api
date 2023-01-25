cfb27617ee596fcd6156078652557785c3954e88
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-01-25 15:18:41 -0300

    change fake operations

--- src/db/seeders/20221116233438-generate-operations.js
+++ src/db/seeders/20221116233438-generate-operations.js
@@ -15,8 +15,8 @@ module.exports = {
       }
       const isOutflow = operation.type === 'outflow'
       operation.amount = isOutflow
-        ? faker.datatype.number({ min: -30, max: -1 })
-        : faker.datatype.number({ min: 1, max: 30 })
+        ? faker.datatype.number({ min: 1 }) * -1
+        : faker.datatype.number({ min: 1 })
 
       operation.category_id = isOutflow
         ? [1, 2, 3, 4][faker.datatype.number({ min: 0, max: 4 })]
